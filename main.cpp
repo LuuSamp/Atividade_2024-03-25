841b70fd00dbc040eddfd3ff4005c4ef5b6498bc
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2024-03-25 08:42:46 -0300

    progressao

--- main.cpp
+++ main.cpp
@@ -33,5 +33,21 @@ int main()
 {
     cout << "Valor absoluto de -7: " << fAbsolute(7) << endl;
     cout << "Fatorial de 5: " << iFatorial(5) << endl;
+
+
+    int iPrimeiroTermo = 1; // Primeiro termo da PA
+    int iRazao = 2; // Razão da PA
+    int iNumTermos = 10; // Número de termos que queremos calcular
+
+    // Chamada da função usando as referências
+    calcularPA(iPrimeiroTermo, iRazao, iNumTermos);
+
     return 0;
+}
+
+void calcularPA(int iRefPrimeiroTermo, int iRefRazao, int iNumTermos) {
+    for(int iCount = 0; iCount < iNumTermos; iCount++) {
+        cout << iRefPrimeiroTermo + iCount * iRefRazao << " ";
+    }
+    cout << endl;
 }
