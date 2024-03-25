98f5b63f177d5a2da7ef0f3c80989d37d3e6db50
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2024-03-25 08:02:48 -0300

    Função fAbsoluto

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+//Reescrever uma função matemática (abs ou pow)
+//Fatorial Iterativo
+//Progressão Aritmética
+//Fibonacci Iterativo
+//Cálculo do Número de Dígitos de um Inteiro
+
 #include <iostream>
 
 using std::cin;
@@ -5,8 +11,17 @@ using std::cout;
 using std::endl;
 using std::string;
 
+float fAbsolute(float fNumero){
+    if(fNumero >= 0){
+        return fNumero;
+    }
+    else {
+        return -fNumero;
+    }
+}
+
 int main()
 {
-    
+    cout << "Valor absoluto de -7: " << fAbsolute(7) << endl;
     return 0;
 }
