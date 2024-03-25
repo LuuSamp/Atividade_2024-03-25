fb13dac7f32630eab941789ec2f55f5606c794cb
JOÃO GABRIEL MACHADO <B51065@fgv.edu.br>
2024-03-25 08:44:55 -0300

    Fibonacci

--- main.cpp
+++ main.cpp
@@ -29,18 +29,26 @@ int iFatorial(int iNumero){
     return iNumero;
 }
 
+void calcularPA(int iRefPrimeiroTermo, int iRefRazao, int iNumTermos);
+void fibonacciIter(int arr[], int iNumTermos);
+
 int main()
 {
     cout << "Valor absoluto de -7: " << fAbsolute(7) << endl;
     cout << "Fatorial de 5: " << iFatorial(5) << endl;
 
 
-    int iPrimeiroTermo = 1; // Primeiro termo da PA
-    int iRazao = 2; // Razão da PA
-    int iNumTermos = 10; // Número de termos que queremos calcular
+    int iPrimeiroTermo = 1;
+    int iRazao = 2; 
+    int iNumTermos = 10; 
 
     // Chamada da função usando as referências
     calcularPA(iPrimeiroTermo, iRazao, iNumTermos);
+
+     int iNumTermos = 10; // Número de termos que queremos calcular
+    int arr[iNumTermos]; // Array para armazenar os termos
+
+    fibonacciIter(arr, iNumTermos);
 
     return 0;
 }
@@ -48,6 +56,21 @@ int main()
 void calcularPA(int iRefPrimeiroTermo, int iRefRazao, int iNumTermos) {
     for(int iCount = 0; iCount < iNumTermos; iCount++) {
         cout << iRefPrimeiroTermo + iCount * iRefRazao << " ";
+    }
+    cout << endl;
+}
+
+void fibonacciIter(int arr[], int iNumTermos) {
+    arr[0] = 0;
+    arr[1] = 1;
+    
+    for(int i = 2; i < iNumTermos; i++) {
+        arr[i] = arr[i-2] + arr[i-1];
+    }
+    
+    // Imprimir os termos
+    for(int i = 0; i < iNumTermos; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
