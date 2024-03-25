a80f2600068bd90201dd0368122cc9c6322e2223
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2024-03-25 08:08:32 -0300

    Função iFatorial

--- main.cpp
+++ main.cpp
@@ -19,9 +19,19 @@ float fAbsolute(float fNumero){
         return -fNumero;
     }
 }
+int iFatorial(int iNumero){
+    for(int i = iNumero-1; i > 0; i--){
+        iNumero = iNumero * i;
+    }
+    if(iNumero == 0){
+        iNumero = 1;
+    }
+    return iNumero;
+}
 
 int main()
 {
     cout << "Valor absoluto de -7: " << fAbsolute(7) << endl;
+    cout << "Fatorial de 5: " << iFatorial(5) << endl;
     return 0;
 }
